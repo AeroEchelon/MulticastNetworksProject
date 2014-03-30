e0fe966a349f02b154554b9d046f018d8c089f3d
Marvin Bernal <bernal.marvin@gmail.com>
2014-03-30 17:58:55 -0400

    Change the concept of a link.
    It is essentially a high level Socket which also contains the information to send via a port contained in a destination listening socket.

--- src/Link.java
+++ src/Link.java
@@ -3,9 +3,7 @@
  */
 
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketTimeoutException;
 
 /**
  * Provides a link between nodes.
@@ -14,7 +12,7 @@ import java.net.SocketTimeoutException;
  *
  * The source node is assumed to be the SERVER while the destination node is assumed to be the CLIENT node.
  */
-public class Link extends Thread {
+public class Link extends Thread{
 
 
     public static final int     DEFAULT_PORT = 6066;        // Default listening port
@@ -24,34 +22,42 @@ public class Link extends Thread {
 
     private Node                mSourceNode;
     private Node                mDestinationNode;
-    private int                 mPort;
+    private int                 mConnectionPort;
     private int                 mCost;
+    private static int          mLinkID = 0;
 
-    private ServerSocket        mServerListeningSocket;  // Socket to listen for incoming connections
-    private Socket              mTransmissionSocket;
+    private Socket              mSocket;                     // Socket to connect to server
+    private String              mMessageToSend;
 
-    public Link(Node sourceNode, Node destinationNode){
-        this(sourceNode, destinationNode, DEFAULT_PORT, DEFAULT_COST);
+    public Link(){
+        // default constructor used simply for reference purposes. Note this does not increase the LinkID variable.
+
     }
-    
-    public Link(Node sourceNode, Node destinationNode, int port, int cost) {
+
+    public Link(Node sourceNode, Node destinationNode, int connectionPort,  int cost) {
         mSourceNode = sourceNode;
         mDestinationNode = destinationNode;
-        mPort = port;
+        mListeningPort = listeningPort;
+        mConnectionPort = connectionPort;
         mCost = cost;
+        mLinkID++;
+        System.out.println("A new link has been created connecting Node " + mSourceNode.getRouterID() + " to Node " + mDestinationNode.getRouterID() + " over Link " + mLinkID);
     }
 
-    public void setCost(int mCost) {
-        this.mCost = mCost;
+    public void setCost(int cost) {
+        mCost = cost;
     }
-    public void setPort(int mPort) {
-        this.mPort = mPort;
+    public void setPort(int port){
+        mConnectionPort = port;
     }
     public void setDestinationNode(Node mDestinationNode) {
         this.mDestinationNode = mDestinationNode;
     }
     public void setSourceNode(Node mSourceNode) {
         this.mSourceNode = mSourceNode;
+    }
+    public void setMessageToSend(String mMessageToSend) {
+        this.mMessageToSend = mMessageToSend;
     }
 
     public Node getSourceNode() {
@@ -60,59 +66,51 @@ public class Link extends Thread {
     public Node getDestinationNode() {
         return mDestinationNode;
     }
-    public int getPort() {
-        return mPort;
+    public int  getPort() {
+        return mConnectionPort;
     }
-    public int getCost() {
+    public int  getCost() {
         return mCost;
     }
-
+    public int getLinkID(){
+        return mLinkID;
+    }
 
     public void run() {
-
-        while(true){
-            try{
-                initializeConnection();
-                String stringToTransmit = listenForString();
-
-                System.out.println("MESSAGE RECEIVED: " + stringToTransmit);
-                transmitAString(stringToTransmit);
-            }catch (IOException e){
-                e.printStackTrace();
-                break;
-            }
-        }
+        initializeConnection();
+        transmitString();
     }
 
     private void initializeConnection(){
-        try{
-            mServerListeningSocket = new ServerSocket(mPort);
-            mServerListeningSocket.setSoTimeout(SOCKET_TIMEOUT);
 
-            System.out.println("Router " + getSourceNode().getRouterID() + " has connected.");
-
-            System.out.println("Listening on port " + mServerListeningSocket.getLocalPort());
-            mTransmissionSocket =  mServerListeningSocket.accept(); // blocks until a connection is made
-
-            System.out.println("Connected to " + mTransmissionSocket.getRemoteSocketAddress());
-        }catch(SocketTimeoutException socketTimeoutException){
-
-            System.out.println("Socket timed out!");
-
-        }catch(IOException iOException){
-            iOException.printStackTrace();
+        try {
+            mSocket = new Socket(Node.LOCAL_HOST, mConnectionPort);
+            System.out.println("Link " + mLinkID + " as been initialized and Node " + mSourceNode.getRouterID() + " is connected to Node " + mDestinationNode.getRouterID() + " on address " + mSocket.getRemoteSocketAddress());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public void transmitAString(String messageToSend) throws IOException {
-        OutputStream outputStream = mTransmissionSocket.getOutputStream();
-        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-        dataOutputStream.writeUTF(messageToSend);
+    public void transmitString(){
+
+        try {
+            System.out.println("Node " + mSourceNode.getRouterID() + " is using NodeThis is Link " + mLinkID + " being used to transmit data.");
+
+            OutputStream outToServer = mSocket.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+            out.writeUTF("Hello from " + mSocket.getLocalSocketAddress());
+            InputStream inFromServer = mSocket.getInputStream();
+            DataInputStream in = new DataInputStream(inFromServer);
+            System.out.println("Server says " + in.readUTF());
+            OutputStream outputStream = mSocket.getOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+            dataOutputStream.writeUTF(mMessageToSend);
+            mSocket.close();
+        } catch (IOException e) {
+
+            System.out.println("IO Exception!");
+            e.printStackTrace();
+        }
     }
 
-    public String listenForString() throws IOException {
-        InputStream inputStream = mTransmissionSocket.getInputStream();
-        DataInputStream dataInputStream = new DataInputStream(inputStream);
-        return dataInputStream.readUTF();
-    }
 }
